2d74db3f44e308925126714ebf3b5895152a6aff
jordantori08 <jordantori08@onlylaughs.co>
2025-03-03 18:16:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ document.getElementById('search-input').addEventListener('input', function() {
     }
   });
 });
+// Toggle side navigation (genie effect)
+function toggleNav() {
+  const sideNav = document.getElementById("mySidenav");
+  const content = document.getElementById("mainContent");
+
+  // Open or close the side navigation
+  if (sideNav.classList.contains("open")) {
+    sideNav.classList.remove("open");
+    content.classList.remove("shift");
+  } else {
+    sideNav.classList.add("open");
+    content.classList.add("shift");
+  }
+}
