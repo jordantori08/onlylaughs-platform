d527444ae515a479f64eadad0f5a78d46e4792b2
jordantori08 <jordantori08@onlylaughs.co>
2025-03-03 19:22:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,13 @@ function toggleSidebar() {
   // Adjust content margin when the sidebar is hidden
   content.classList.toggle("shift");
 }
+// Get the category from the URL
+const urlParams = new URLSearchParams(window.location.search);
+const category = urlParams.get('category');
+
+// Update the category header on the secondary page
+if (category) {
+  const categoryHeader = document.getElementById('category-header');
+  categoryHeader.textContent = category.replace(/-/g, ' ').toUpperCase(); // Format the category text
+}
 
