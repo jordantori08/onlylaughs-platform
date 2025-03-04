ed0129e27f358a7f264101ca34e23e637acea342
jordantori08 <jordantori08@onlylaughs.co>
2025-03-03 18:18:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-document.getElementById('search-input').addEventListener('input', function() {
-  let searchTerm = this.value.toLowerCase();
-  let videos = document.querySelectorAll('.video-item p');
-
-  videos.forEach(function(video) {
-    let text = video.innerText.toLowerCase();
-    if (text.includes(searchTerm)) {
-      video.parentElement.style.display = 'block';
-    } else {
-      video.parentElement.style.display = 'none';
-    }
-  });
-});
 // Toggle side navigation (genie effect)
 function toggleNav() {
   const sideNav = document.getElementById("mySidenav");
